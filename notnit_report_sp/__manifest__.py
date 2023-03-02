7fadb609e17adcd2f4fb2b2abfc1524ab5429460
tintonajisadewo <tintonajisadewo@gmail.com>
2023-03-02 13:26:24 +0700

    update

--- notnit_report_sp/__manifest__.py
+++ notnit_report_sp/__manifest__.py
@@ -3,21 +3,21 @@
 
 
 {
-    'name': 'Report QWEB Invoice and Delivery Slip Add Signature',
+    'name': 'notnit_report_sp',
     'author': 'Tinton Aji Sadewo',
-    'version': '16.0.0',
+    'version': '14.0.0',
     "license": "AGPL-3",
-    'category': 'Report QWEB Invoice and Delivery Slip',
+    'category': 'Report Invoice and Delivery Slip',
     'price': '15.0',
-    'currency':'USD',
+    'currency': 'USD',
     'summary': """
-       Report QWEB Invoice and Delivery Slip Add Signature""",
+        Add Signature in report sale invoice and Delivery Slip""",
     'description': """
-Report QWEB Invoice and Delivery Slip Add Signature
+Add Signature in report sale invoice and Delivery Slip
 """,
     'website': 'https://tintonajisadewo.github.io/',
-    'images': ['static/description/__manifest__.png',],
-    'depends': ['sale','stock'],
+    'images': ['static/description/__manifest__.png', ],
+    'depends': ['sale', 'stock', ],
     'data': [
         'report/notnit_report_sp_head.xml',
         'report/report_notnit_report_sp_doc.xml',
